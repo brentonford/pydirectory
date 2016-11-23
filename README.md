0ea4ac436ada2e3749e91d10d1097ceff1e31332
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-11-23 22:25:33 +1000

    First commit of Directory class

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # pydirectory
 A class representing a file system directory, that deletes on garbage collect.
+
+Required:
+* Python 3.5
+
+The Directory object can be 
+* Initialised on an existing directory
+* Scan that directory
+* Be interigated for the file list
+* Create new files
+* Open files
+
+It can also be used when a temporary directory of files is needed, that will be garbage
+collected when it falls out of scope.
+
+This is especially usefull when using the tarfile, etc packages.
---
+++ src/pydirectory/Directory.py
@@ -0,0 +1,259 @@
+"""
+ * Created by Synerty Pty Ltd
+ *
+ * This software is open source, the MIT license applies.
+ *
+ * Website : http://www.synerty.com
+ * Support : support@synerty.com
+"""
+
+import tempfile
+import shutil
+import weakref
+from subprocess import check_output
+
+import os
+
+
+class DirSettings:
+    tmpDirPath = '/tmp'
+    defaultDirChmod = 0x700
+
+
+__author__ = 'synerty'
+
+textchars = bytearray([7, 8, 9, 10, 12, 13, 27]) + bytearray(list(range(0x20, 0x100)))
+is_binary_string = lambda bytes_: bool(bytes_.translate(None, textchars))
+
+
+class FileDisappearedError(Exception):
+    pass
+
+
+class Directory(object):
+    def __init__(self, initWithDir=None, autoDelete=True, inDir=None):
+        self._files = {}
+        self._autoDelete = autoDelete
+
+        if initWithDir:
+            self.path = initWithDir
+            self.scan()
+
+        else:
+            self.path = tempfile.mkdtemp(dir=(inDir if inDir else DirSettings.tmpDirPath))
+
+        closurePath = self.path
+
+        def cleanup(me):
+            if autoDelete:
+                shutil.rmtree(closurePath)
+
+        self.__cleanupRef = weakref.ref(self, cleanup)
+
+    @property
+    def files(self):
+        """ Files
+        :return: A list of the Directory.File objects
+        """
+        return list(self._files.values())
+
+    @property
+    def pathNames(self):
+        """ Path Names
+        :return: A list of path + name of each file, relative to the directory root
+        """
+        return [f.pathName for f in list(self._files.values())]
+
+    @property
+    def paths(self):
+        """ Paths
+        :return: A list of the path names, effectively a list of relative directory
+                  names
+        """
+        return set([f.path for f in list(self._files.values())])
+
+    @property
+    def tableOfContents(self):
+        return [f.archiveContentItem for f in self.files]
+
+    def getFile(self, path='', name=None, pathName=None):
+        assert (name or pathName)
+        pathName = (pathName if pathName else os.path.join(path, name))
+        return self._files.get(pathName)
+
+    def createFile(self, path="", name=None, pathName=None):
+        file = File(self, path=path, name=name, pathName=pathName)
+        self._files[file.pathName] = file
+        return file
+
+    def createHiddenFolder(self):
+        if not self._autoDelete:
+            raise Exception("Hidden folders can only be created within"
+                            " an autoDelete directory")
+        return tempfile.mkdtemp(dir=self.path, prefix=".")
+
+    def scan(self):
+        self._files = {}
+        find = "find %s -type f" % self.path
+        output = check_output(args=find.split()).strip().split('\n')
+        output = [line for line in output if not "__MACOSX" in line]
+        for pathName in output:
+            if not pathName:  # Sometimes we get empty lines
+                continue
+
+            pathName = pathName[len(self.path) + 1:]
+            file = File(self, pathName=pathName, exists=True)
+            self._files[file.pathName] = file
+
+        return self.files
+
+    def clone(self, autoDelete=True):
+        d = Directory(autoDelete=autoDelete)
+        os.rmdir(d.path)  # shutil doesn't like it existing
+        shutil.copytree(self.path, d.path)
+        d.scan()
+        return d
+
+    def _fileDeleted(self, file):
+        self._files.pop(file.pathName)
+
+    def _fileMoved(self, oldPathName, file):
+        self._files.pop(oldPathName)
+        self._files[file.pathName] = file
+
+
+class File(object):
+    def __init__(self, directory, path='', name=None, pathName=None,
+                 exists=False):
+        assert (isinstance(directory, Directory))
+        assert (name or pathName)
+
+        self._directory = weakref.ref(directory)
+
+        if name:
+            path = path if path else ''
+            self._pathName = os.path.join(path, name)
+
+        elif pathName:
+            self._pathName = pathName
+
+        self._pathName = self.sanitise(self._pathName)
+
+        if exists != os.path.exists(self.realPath):
+            raise FileDisappearedError(self.realPath)
+
+        if not os.path.exists(self.realPath):
+            with self.open(append=True):
+                os.utime(self.realPath, None)
+                os.chmod(self.realPath, 0o600)
+
+    # ----- Name and Path setters
+    @property
+    def path(self):
+        return os.path.dirname(self.pathName)
+
+    @path.setter
+    def path(self, path):
+        path = path if path else ''
+        self.pathName = os.path.join(path, self.name)
+
+    @property
+    def name(self):
+        return os.path.basename(self.pathName)
+
+    @name.setter
+    def name(self, name):
+        self.pathName = os.path.join(self.path, name)
+
+    # ----- pathName functions
+
+    @property
+    def pathName(self):
+        return self._pathName
+
+    @pathName.setter
+    def pathName(self, pathName):
+        if self.pathName == pathName:
+            return
+
+        pathName = self.sanitise(pathName)
+        before = self.realPath
+        after = self._realPath(pathName)
+
+        assert (not os.path.exists(after))
+
+        newRealDir = os.path.dirname(after)
+        if not os.path.exists(newRealDir):
+            os.makedirs(newRealDir, DirSettings.defaultDirChmod)
+
+        shutil.move(before, after)
+
+        oldPathName = self._pathName
+        self._pathName = pathName
+
+        self._directory()._fileMoved(oldPathName, self)
+
+    # ----- Util functions
+
+    def open(self, append=False, write=False):
+        flag = {(False, False): 'r',
+                (True, False): 'a',
+                (True, True): 'a',
+                (False, True): 'w'}[(append, write)]
+
+        realPath = self.realPath
+        realDir = os.path.dirname(realPath)
+        if not os.path.exists(realDir):
+            os.makedirs(realDir, DirSettings.defaultDirChmod)
+        return open(self.realPath, flag)
+
+    def delete(self):
+        directory = self._directory()
+        assert isinstance(directory, Directory)
+
+        realPath = self.realPath
+        assert (os.path.exists(realPath))
+        os.remove(realPath)
+
+        directory._fileDeleted(self)
+
+    def remove(self):
+        """ Remove
+        Removes the file from the Directory object, file on file system remains on disk
+        """
+        directory = self._directory()
+        assert isinstance(directory, Directory)
+        directory._fileDeleted(self)
+
+    @property
+    def size(self):
+        return os.stat(self.realPath).st_size
+
+    @property
+    def mtime(self):
+        return os.path.getmtime(self.realPath)
+
+    @property
+    def isContentText(self):
+        with self.open() as f:
+            return not is_binary_string(self.open().read(40000))
+
+    @property
+    def realPath(self):
+        return self._realPath()
+
+    def _realPath(self, newPathName=None):
+        directory = self._directory()
+        assert (directory)
+        return os.path.join(directory.path,
+                            newPathName if newPathName else self._pathName)
+
+    def sanitise(self, pathName):
+        assert isinstance(pathName, str)
+        assert not '..' in pathName
+        assert not pathName.endswith(os.sep)
+
+        while pathName.startswith(os.sep):
+            pathName = pathName[1:]
+
+        return pathName
