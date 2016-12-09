fea311405565d69469280974df759848aa07e2c4
brentonford <brenton.ford@synerty.com>
2016-12-09 15:54:59 -0500

    experimenting with document builders.
    bat file works perfectly on windows.
    
    yet to run the bash shell script.

--- docs/build_pytmpdir_docs.bat
+++ docs/build_pytmpdir_docs.bat
@@ -1,14 +1,12 @@
-@echo off
 title Build pytmpdir docs!
 echo Building documentation for pytmpdir!
 echo Removing old documentation in build folder.
 rmdir build /s /q
 echo Updating module rst files.  This will overwrite old rst files.
-rmdir source\scripts /s /q
-mkdir source\scripts
-copy ..\pytmpdir\Directory.py source\scripts
-sphinx-apidoc -f -o source source\scripts
+cd ..\pytmpdir
+sphinx-apidoc -f -o ..\docs\source .
 echo Build HTML files.
+cd ..\docs
 sphinx-build -b html source build
 echo Opening created documentation...
 start build\index.html
