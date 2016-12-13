d85aa0a9b5c735f72de183af7457f99afa64db8a
brentonford <brenton.ford@synerty.com>
2016-12-13 15:55:19 -0500

    relocated shell script that creates the documentation to the project directory

--- build_pytmpdir_docs.sh
+++ build_pytmpdir_docs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "Building Sphinx documentation for pytmpdir!"
 echo "Removing old documentation in build folder."
-rm -frv build
+rm -frv docs/build
 echo "Updating module rst files.  This will overwrite old rst files."
-set PYTHONPATH="C:\Users\brentonford\Documents\ORMOB\pytmpdir"
-sphinx-apidoc -f -o ../docs/source ../pytmpdir
+export PYTHONPATH="`pwd`"
+sphinx-apidoc -f -o docs/source pytmpdir
 echo "Build HTML files."
-sphinx-build -b html source build
+sphinx-build -b html docs/source docs/build
 echo "Opening created documentation..."
-start build/index.html
+start docs/build/index.html
