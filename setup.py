83decee9de424257bded849b4e97fb985f8dbb62
brentonford <brenton.ford@synerty.com>
2016-12-13 13:28:33 -0500

    bat file replaced by shell script.
    pydirectory renaming scheme updated for pypi setup files.

--- setup.py
+++ setup.py
@@ -2,19 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytmpdir',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # this
-    # must be the same as the name
-    # above
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     version='0.1.0',
     description='A class representing a file system directory, that deletes on '
                 'garbage collect.',
     author='Synerty',
     author_email='support@synerty.com',
-    url='https://github.com/Synerty/pydirectory',
-    # use the URL to the github repo
-    download_url='https://github.com/Synerty/pydirectory/tarball/0.1.0',
-    # I'll explain this in a second
+    url='https://github.com/Synerty/pytmpdir',
+    download_url='https://github.com/Synerty/pytmpdir/tarball/0.1.0',
     keywords=['directory', 'scan', 'interrogate', 'create', 'open'],
-    # arbitrary keywords
     classifiers=[],
 )
